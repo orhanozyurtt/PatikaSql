a33992e4172e30b13f7ecd1968ba54d4ee503790
orhan oz <orhanozyurt@outlook.com>
2023-06-03 16:56:25 +0300

    homework 8 added

---
+++ homework8/Solution3.sql
@@ -0,0 +1,30 @@
+UPDATE employee 
+SET name = 'updateName' 
+where id = 1;
+
+UPDATE employee 
+SET birthday = '2002-05-03' 
+where birthday = '2006-02-16';
+
+UPDATE employee 
+SET email = 'danDanita@email.com' 
+WHERE name = 'Dqanita';
+
+UPDATE employee 
+SET name = 'murat'
+WHERE id = 5;
+
+UPDATE employee
+SET birthday = '2000-01-28'
+WHERE name = 'Harper';
+
+UPDATE employee 
+SET name = 'Demir'
+WHERE id = 11;
+
+
+
+
+
+
+
---
+++ homework8/Solution4.sql
@@ -0,0 +1,18 @@
+DELETE FROM employee
+WHERE id = 5; 
+
+DELETE FROM employee
+WHERE name = 'Tisha'; 
+
+DELETE FROM employee
+WHERE birthday = '2022-03-23'; 
+
+DELETE FROM employee
+WHERE name = 'murat';
+
+DELETE FROM employee
+WHERE birthday = '2000-01-28'; 
+
+
+
+
